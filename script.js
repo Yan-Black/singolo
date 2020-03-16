d5bbcdd8df59841e9cb6c37410c6e7d658461fff
Yan Andryshkin <yan.7feb@gmail.com>
2020-03-16 13:11:07 +0300

    fix: fix img border & popup text

--- script.js
+++ script.js
@@ -8,9 +8,10 @@ let nav        = document.querySelector('.navigation'),
     portImages = document.querySelectorAll('.portfolio__gallery > img'),
     submit     = document.getElementById('submit'),
     closeBtn   = document.querySelector('.close-btn') 
-    position   = ['-1020px','1020px']
+    position   = [`-${slider.getBoundingClientRect().width}px`,`${slider.getBoundingClientRect().width}px`]
 
 nav.addEventListener('click', e => {
+
     let target = e.target
         if(target.tagName != 'A') return
 
@@ -19,30 +20,45 @@ nav.addEventListener('click', e => {
 }) 
 
 slider.addEventListener('click', e => {
-    let target = e.target   
-        if(target.className != 'phone') return 
 
+    let target = e.target  
+
+        if(target.className === 'phone__screen' || target.className === 'phone__screen phone__screen_horizontal') target.classList.add('phone__screen_off')
+        if(target.className !== 'phone') return 
         target.parentElement.lastElementChild.classList.toggle('phone__screen_off')
 }) 
 
+arrows[0].addEventListener('click', () => {
+    changeInitPosition(position[0])
+    setTimeout(() => changeSlide(position[1]),100)
+})
+
+arrows[1].addEventListener('click', () => {
+    changeInitPosition(position[1])
+    setTimeout(() => changeSlide(position[0]),100)
+})
+
 portNav.addEventListener('click', e => {
+
     let target = e.target
         if(target.tagName != 'A') return
 
         portLinks.forEach(el => el.classList.remove('portfolio-active'))
         target.classList.add('portfolio-active')
-        portImages.forEach(el => el.style.order = Math.trunc(Math.random() * 10))
+        //Change img position
+        portImages.forEach(el => el.style.order = Math.trunc(Math.random() * 12))   
 }) 
 
 portfolio.addEventListener('click', e => {
-    let target = e.target
-        if(target.tagName != 'IMG') {
-            portImages.forEach(el => el.classList.remove('img-selected'))
-            return
-        }
 
+    let target = e.target
+        if(target.tagName != 'IMG') return
+        if(target.className === 'img-selected') {
+           target.classList.remove('img-selected')
+           return
+        } 
         portImages.forEach(el => el.classList.remove('img-selected'))
-        target.classList.toggle('img-selected')
+        target.classList.add('img-selected')
 })
 
 submit.addEventListener('click', () => {
@@ -59,33 +75,29 @@ submit.addEventListener('click', () => {
 
     if(!name || !email || !regexp.test(name)) return
 
-    function createText(sub, text) {
-
-        message__text.innerText = ''
-        message__text.innerText = `Тема:  ${sub} 
-                                   Описание:  ${text}`
-
-        if(!sub)          message__text.innerText = `Без темы 
-                                                     Описание:  ${text}`
-
-        if(!text)         message__text.innerText = `Тема:  ${sub}
-                                                     Без описания `
-
-        if(!sub && !text) message__text.innerText = `Без темы
-                                                     Без описания`
-    }
-
-     createText(subject, textarea)
+    createText(subject, textarea)
 
     mess.classList.toggle('hidden')
 })
 
 closeBtn.addEventListener('click', () => {
+
     let messageBlock = document.getElementById('message-block')
-    messageBlock.classList.toggle('hidden')
+        messageBlock.classList.toggle('hidden')
 })
 
-function changeSlide(pos) {
+const createText = (sub, text) => {
+
+    message__head.innerText = ''
+    message__text.innerText = ''
+    sub  ? message__head.innerText = `Тема: ${sub}`      : message__head.innerText = `Без темы`
+    text ? message__text.innerText = `Описание: ${text}` : message__text.innerText = `Без описания`
+}
+
+const changeSlide = pos => {
+
+    let delay = parseFloat(getComputedStyle(slides[0]).transition.slice(4,7))*1000
+         
     slides[0].style.left = pos 
     slides[1].style.left = '0px'
     slides = slides.reverse(); 
@@ -93,27 +105,19 @@ function changeSlide(pos) {
             slides[1].remove();
             slides[1].style.left = pos;
             slider.append(slides[1]);
-        },500)
+        },delay)
 }
 
-function changeInitPosition(pos){
-    slides.forEach(el => el.classList.add('animate'))
+const changeInitPosition = pos => {
+
+    slides.forEach(el => el.classList.add('animate'))             
     if(slides[1].style.left !== pos) {
         setTimeout(() => {
             slides[1].remove();
             slides[1].style.left = pos;
             slider.append(slides[1]);
-        },100)
+        },0)
     }
 }
 
-arrows[0].addEventListener('click', function(){
-    changeInitPosition(position[0])
-    setTimeout(() => changeSlide(position[1]),120)
-})
-
-arrows[1].addEventListener('click', function(){
-    changeInitPosition(position[1])
-    setTimeout(() => changeSlide(position[0]),120)
-})
 
